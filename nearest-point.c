770baf3911036be619f9bedf02007590fd28a0c6
padam damai <padamdamai888@gmail.com>
2023-06-07 15:39:30 +0545

    added code:smallest gap between number

--- nearest-point.c
+++ nearest-point.c
@@ -1,6 +1,7 @@
 
 #include<stdio.h>
-int num,i;
+int num,i,j;
+int value = 100000;
 float x = 4.2;
 float arr2[76],array[78],largest = 0;
 int main() {
@@ -25,17 +26,23 @@ for(i=0;i<num;i++){
  for(i=0;i<num;i++){
    printf("%.2f\n",arr2[i]);
  }
-// for(i=0;i<num;i++){
-// if(smallest)
-// }
-// float less_gap =0 ;
-// for(i=0;i<num;i++){
-//     for(i=0;i<num;i++){
-//                 less_gap= arr2[i]- less_gap;
+for(i=0;i<num;i++){
+  for(j=1;j<num;j++){
+  if(arr2[i] >= array[i]){
+        if((arr2[i] - array[i]) < value){
+          value = arr2[i] - array[i];
+  }
+  else{
+    if((array[i] - arr2[i]) < value){
+          value = array[i] - arr2[i];
+  }
+  }
+}
+}
+}
+printf("The smallest gap between the number is %.2f\n",value);
 
-//         }
 
-// }
     return 0;
 }
 
