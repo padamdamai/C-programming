1f05fdef71e98383c9cf90bb9b62a0271167d036
padamdamai <72964773+padamdamai@users.noreply.github.com>
2023-06-13 13:19:53 +0545

    Update nearest-point.c

--- nearest-point.c
+++ nearest-point.c
@@ -1,55 +1,67 @@
-
 #include<stdio.h>
 #include<stdlib.h>
 int total,i;
-int max=10000000,min=(-10000000), array[78];
-int count = 0;
+int max=10000000,min=(-10000000), array[78], max_num=0,min_num=(1000000000);
+//int count = 0,count_number=0;
 float point;
 int main() {
 printf("How many number you want to entered:");
 scanf("%d",&total);
 
 if(total != 0){
-printf("Which nearest point you want to find,\n Enter the point:");
-scanf("%f",&point);
-printf("Enter the number:\n");
-for( i=0 ; i < total ; i++ ){
-      scanf("%d",&array[i]);
- }
+		printf("Which nearest point you want to find,\n Enter the point:");
+		scanf("%f",&point);
+		printf("Enter the number:\n");
+		for( i=0 ; i < total ; i++ ){
+		      scanf("%d",&array[i]);
+		 }
+ 
+				 // to find maxiimum and minimum number
+				 for( i=0 ; i < total ; i++ ){
+				      if(array[i] > max_num ){
+				      	max_num = array[i];
+					  }
+					   if(array[i] < min_num ){
+				      	min_num = array[i];
+					  }
+				 }
+//printf("\nThe max and min is %d and %d",min_num,max_num);
 
-for(i=0;i<total;i++){
-  int a = array[i];
-if (a < point){
-    count++;
-}
-}
-if(count == 0){
-   printf("!!!PLEASE ENTER THE NEAREST POINT PROPERLY\n");
-  exit(0);
-}
+				for(i=0;i<total;i++)
+				{
+				      if((array[i] > point) &&  (array[i] < max))
+				      {
+				     max = array[i];
+				    
+				      }
+				      
+				      if((array[i] < point) &&  (array[i]> min))
+				      {
+				        min = array[i];
+				      }
+				 }
 
-for(i=0;i<total;i++)
-{
-      if((array[i] > point) &&  (array[i] < max))
-      {
-     max = array[i];
-    
-      }
-      
-      if((array[i] < point) &&  (array[i]> min))
-      {
-        min = array[i];
-      }
- }
-  printf("\nThe nearest point of  is %d and %d",min,max);
 }
 else{
-  printf("S!!!!! sorry please enter the number");
+	exit(0);
+}
+
+	
+if((point > min_num) && (point < max_num))
+			{
+      						 if(min == (-10000000) )
+							   {
+ 							printf("The nearest point is %d",max);
+		 						}
+ 							else
+							 {
+ 							printf("The nearest point is %d and %d",min,max);
+ 							}
+	  		}
+
+else{
+	printf("\n\n+!!!Error Message \nPLEASE ENTER THE NEAREST POINT PROPERLY\n");
 }
 
  return 0;
 }
-
-
-
-
